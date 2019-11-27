2752ec8210e8ec710218b90b447350db4d80af2c
Sam <ussamuel88@gmail.com>
2019-11-26 19:11:47 -0800

    committing

--- polls/views.py
+++ polls/views.py
@@ -14,9 +14,7 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id
-
-from django.http import HttpResponse
+    return HttpResponse("You're voting on question %s." % question_id)
 
 from .models import Question
 
